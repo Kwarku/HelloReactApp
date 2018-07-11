0735ad7d61b89cfc2c16fcf37252db2d2d77db6a
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-11 14:01:06 +0200

    add state elemtent into App component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    pesrons:[
+      {name:'Paweł', age: 25},
+      {name:'Karol', age: 12},
+      {name:'Krystian', age: 35},
+      {name:'Michał', age: 27},
+    ]
+  };
+  
   render() {
         return (
          <div className="App">
            <h1>Hi, this is my first React App</h1>
            <p> This is some text</p>
-           <Person name="Paweł" age="23"/>
-           <Person name="Karol" age="12"/>
-           <Person name="Krystian" age="35">My hobby is Math </Person>
-           <Person name="Michał" age="27"/>
+           <button>Change Person </button>
+           <Person name={this.state.pesrons[0].name} age={this.state.pesrons[0].age}/>
+           <Person name={this.state.pesrons[1].name} age={this.state.pesrons[1].age}/>
+           <Person name={this.state.pesrons[2].name} age={this.state.pesrons[2].age}>My hobby is Math </Person>
+           <Person name={this.state.pesrons[3].name} age={this.state.pesrons[3].age}/>
          </div>
     );
   }
