a470059a2b3f2294b580785cf757159cf12db16a
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-11 18:50:27 +0200

    add dynamic name change method in App and use in to Person component

--- src/App.js
+++ src/App.js
@@ -12,40 +12,34 @@ class App extends Component {
     ]
   };
 
-  changeNameHandler = (newName) => {
-    this.setState({
-      pesrons: [
-        { name: newName, age: 25 },
-        { name: 'Karol', age: 12 },
-        { name: 'Krystian', age: 35 },
-        { name: 'Michał', age: 27 },
-      ]
-    });
-  }
+dynamicChangeNameMethodHandler = (event) =>{
+  this.setState({
+    pesrons:[
+      { name: 'Paweł', age: 25 },
+      { name: event.target.value, age: 12 },
+      { name: 'Krystian', age: 35 },
+      { name: 'Michał', age: 27 },
+    ]
+  });
+}
 
   render() {
     return (
       <div className="App">
         <h1>Hi, this is my first React App</h1>
         <p> This is some text</p>
-        <button
-          // This way using bind method is better. USE IT
-          onClick={this.changeNameHandler.bind(this, 'Pafcio')}>Change Person
-        </button>
-
         <Person
           name={this.state.pesrons[0].name}
           age={this.state.pesrons[0].age} />
 
         <Person
           name={this.state.pesrons[1].name}
-          age={this.state.pesrons[1].age} />
+          age={this.state.pesrons[1].age}
+          change={this.dynamicChangeNameMethodHandler} />
 
         <Person
           name={this.state.pesrons[2].name}
           age={this.state.pesrons[2].age}
-          // This way using () => is worst
-          click={() => this.changeNameHandler('Paweł!111')}
         >My hobby is Math
         </Person>
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const person = (props) => {
     return(
-    <div onClick={props.click}>
+    <div>
         <p>I'm {props.name} and I'm {props.age} year old.</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.change} value={props.name}/>
     </div>
     );
 }
