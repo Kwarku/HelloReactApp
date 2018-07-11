a56f34e1d41536bf2367cd4bcff8496a3350044e
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-11 14:33:38 +0200

    add change state event handler methods in to App component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,28 @@ class App extends Component {
   };
   
 changeNameHandler = () => {
-  console.log("It's clicked!")
+  // console.log("It's clicked!")
+  // DONT'T DO THIS : this.state.pesrons[1].name ='Jakub';
+
+  this.setState({
+    pesrons:[
+      {name:'Michał', age: 27},
+      {name:'Krystian', age: 35},
+      {name:'Karol', age: 12},
+      {name:'Paweł', age: 25},
+    ]
+  });
+}
+
+reChangeNameHandler = ()=>{
+  this.setState({
+    pesrons:[
+      {name:'Paweł', age: 25},
+      {name:'Karol', age: 12},
+      {name:'Krystian', age: 35},
+      {name:'Michał', age: 27},
+    ]
+  });
 }
 
   render() {
@@ -22,6 +43,7 @@ changeNameHandler = () => {
            <h1>Hi, this is my first React App</h1>
            <p> This is some text</p>
            <button onClick={this.changeNameHandler}>Change Person </button>
+           <button onClick={this.reChangeNameHandler}> re change Person </button>
            <Person name={this.state.pesrons[0].name} age={this.state.pesrons[0].age}/>
            <Person name={this.state.pesrons[1].name} age={this.state.pesrons[1].age}/>
            <Person name={this.state.pesrons[2].name} age={this.state.pesrons[2].age}>My hobby is Math </Person>
