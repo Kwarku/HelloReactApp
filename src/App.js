78252861392a73cb6abd0529f387e610a6c64cc2
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-11 12:57:08 +0200

    add Person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
         return (
          <div className="App">
            <h1>Hi, this is my first React App</h1>
+           <p> This is some text</p>
+           <Person />
          </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>Im user</p>
+}
+
+export default person;
