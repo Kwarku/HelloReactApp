1940468ee0d8cd24def8a3c48417b13df65f002e
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-11 11:04:41 +0200

    react render without JSX

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, this is my first React App</h1>
-      </div>
-    );
+    return React.createElement('div', {className:'App'},
+     React.createElement('h1',null,'Hello it\'s me.'));
   }
 }
 
