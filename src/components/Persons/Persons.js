d697774c4b688dfa716bd473bbd7a990996867df
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-16 13:47:14 +0200

    change logic in Persons component
    now shouldComponentUpdate method check if something change in new props then re-render all DOM, if not then return false and do nothing

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -19,8 +19,9 @@ class Persons extends Component{
 
   shouldComponentUpdate(nextProps, nextState){
     console.log('[UPDATE Persons.js] msg from shouldComponentUpdate() ', nextProps , nextState);
-    // return nextProps.persons !== this.props.persons;
-    return true;
+    return nextProps.persons !== this.props.persons ||
+    nextProps.clicked !== this.props.clicked || 
+    nextProps.changed !== this.props.changed;
   }
 
   componentWillUpdate(nextProps,nextState){
